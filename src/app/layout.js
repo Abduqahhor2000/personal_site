44ddab302622635e4341f8b371b791941a2a702e
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2023-02-11 15:13:20 +0500

    push

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,12 @@
-import './globals.css'
+import "./globals.css"
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <div>{children}</div>
+      </body>
     </html>
-  )
+  );
 }
